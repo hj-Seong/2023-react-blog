c726b1ca9c3aebd491a938c0dbb315197753d460
HJ S <hjseong1222@gmail.com>
2023-04-26 11:06:39 +0900

    0426 react-router-dom, react-bootstrap 설치

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+
+// 리액트 부트스트랩을 사용하기 위해서는 css추가 필요
+import  'bootstrap/dist/css/bootstrap.min.css' ;
 import './App.css';
+
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button></Button>
     </div>
   );
 }
